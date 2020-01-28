f36b956c26e9a47de5ffcda266128bbafccd6f01
Mark Allen <allenam@tcd.ie>
2020-01-28 21:03:29 +0000

    fixed commit directory

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,16 +1,18 @@
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     TESTING = True
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
