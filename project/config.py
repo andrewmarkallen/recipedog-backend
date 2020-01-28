b4f79bb3054e2c9d16a14f6e31085f068a180d41
Mark Allen <allenam@tcd.ie>
2020-01-28 09:55:22 +0000

    First commit

---
+++ project/config.py
@@ -0,0 +1,16 @@
+class BaseConfig:
+    """Base Configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development Configuration"""
+    pass
+
+class TestingConfig(BaseConfig):
+    """Testing Configuration"""
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    """Production Configuration"""
+    pass
