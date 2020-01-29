61fda42cf47b92963eb142a0070d928075ff7dc1
Mark Allen <allenam@tcd.ie>
2020-01-29 16:50:32 +0000

    Added postgres docker image

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,15 +1,20 @@
+import os
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
