aab7c197b5a3f2575fc7c21e057dd1ffb860a9df
Mark Allen <allenam@tcd.ie>
2020-01-29 12:48:29 +0000

    Restart

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,18 +1,15 @@
 class BaseConfig:
-    """Base Configuration"""
+    """Base configuration"""
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    pass
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     TESTING = True
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    pass
