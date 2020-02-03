cdb6e1dfcb0dbf865d16ea97c53f749b7a7a5430
Mark Allen <allenam@tcd.ie>
2020-02-03 12:00:22 +0000

    Fixed linting issues

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -8,6 +8,7 @@ from project import create_app
 
 app = create_app()
 
+
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
         app.config.from_object('project.config.DevelopmentConfig')
@@ -17,8 +18,8 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(current_app is None)
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
-            os.environ.get('DATABASE_URL')
+            app.config['SQLALCHEMY_DATABASE_URI']
+            == os.environ.get('DATABASE_URL')
         )
 
 
@@ -32,8 +33,8 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
-            os.environ.get('DATABASE_TEST_URL')
+            app.config['SQLALCHEMY_DATABASE_URI']
+            == os.environ.get('DATABASE_TEST_URL')
         )
 
 
