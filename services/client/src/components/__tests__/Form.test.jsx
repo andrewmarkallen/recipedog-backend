3bc6a615bdba9308ba8eb89a1683b94f5baa2813
Mark Allen <allenam@tcd.ie>
2020-02-20 14:28:04 +0000

    refactored form.test

--- services/client/src/components/__tests__/Form.test.jsx
+++ services/client/src/components/__tests__/Form.test.jsx
@@ -4,44 +4,39 @@ import renderer from 'react-test-renderer'
 
 import Form from '../Form'
 
-const formData = {
-  username: '',
-  email: '',
-  password: '',
-}
+const testData = [
+  {
+    formType: 'Register',
+    formData: {
+      username: '',
+      email: '',
+      password: ''
+    },
+  },
+  {
+    formType: 'Login',
+    formData: {
+      email: '',
+      password: ''
+    },
+  }
+]
 
-test('Register Form renders properly', ()  => {
-  const component = <Form formType={'Register'} formData={formData}/>
-  const wrapper = shallow(component)
-  const h1 = wrapper.find('h1')
-  expect(h1.length).toBe(1)
-  expect(h1.get(0).props.children).toBe('Register')
-  const formGroup = wrapper.find('.form-group')
-  expect(formGroup.length).toBe(3)
-  expect(formGroup.get(0).props.children.props.name).toBe('username')
-  expect(formGroup.get(0).props.children.props.value).toBe('')
-})
-
-test('Login Form renders properly', ()  => {
-  const component = <Form formType={'Login'} formData={formData}/>
-  const wrapper = shallow(component)
-  const h1 = wrapper.find('h1')
-  expect(h1.length).toBe(1)
-  expect(h1.get(0).props.children).toBe('Login')
-  const formGroup = wrapper.find('.form-group')
-  expect(formGroup.length).toBe(2)
-  expect(formGroup.get(0).props.children.props.name).toBe('email')
-  expect(formGroup.get(0).props.children.props.value).toBe('')
-})
-
-test('Register Form renders a snapshot properly', ()  => {
-  const component = <Form formType={'Register'} formData={formData}/>
-  const tree = renderer.create(component).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-test('Login Form renders a snapshot properly', ()  => {
-  const component = <Form formType={'Login'} formData={formData}/>
-  const tree = renderer.create(component).toJSON()
-  expect(tree).toMatchSnapshot()
+testData.forEach((el)  => {
+  test('${el.formType} Form renders properly', ()  => {
+    const component = <Form formType={el.formType} formData={el.formData}/>
+    const wrapper = shallow(component)
+    const h1 = wrapper.find('h1')
+    expect(h1.length).toBe(1)
+    expect(h1.get(0).props.children).toBe(el.formType)
+    const formGroup = wrapper.find('.form-group')
+    expect(formGroup.length).toBe(Object.keys(el.formData).length)
+    expect(formGroup.get(0).props.children.props.name).toBe(Object.keys(el.formData)[0])
+    expect(formGroup.get(0).props.children.props.value).toBe('')
+  })
+  test('${el.formType} Form renders a snapshot properly', ()  => {
+    const component = <Form formType={el.formType} formData={el.formData}/>
+    const tree = renderer.create(component).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
