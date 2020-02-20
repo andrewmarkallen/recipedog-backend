0485069d796ce07b590feab41d178a60fe68ad22
Mark Allen <allenam@tcd.ie>
2020-02-20 14:15:06 +0000

    Added (unwired) form component for register and login

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -6,6 +6,7 @@ import UsersList from './components/UsersList'
 import AddUser from './components/AddUser'
 import About from './components/About'
 import NavBar from './components/NavBar'
+import Form from './components/Form'
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,12 @@ class App extends Component {
       users: [],
       username: '',
       email: '',
-      title: 'RecipeDog'
+      title: 'RecipeDog',
+      formData: {
+        username: '',
+        email: '',
+        password: '',
+      }
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -78,6 +84,18 @@ class App extends Component {
 
             )} />
             <Route exact path='/about' component={About}/>
+            <Route exact path='/register' render={()  => (
+              <Form
+                formType={'Register'}
+                formData={this.state.formData}
+              />
+            )} />
+            <Route exact path='/login' render={()  => (
+              <Form
+                formType={'Login'}
+                formData={this.state.formData}
+              />
+            )} />
           </Switch>
           </div>
         </div>
